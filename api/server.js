38425baff95176a80bd4204a4fccdca273e1013f
MMostella <masonmostella@gmail.com>
2021-09-30 18:54:50 -0500

    Added basic routes and set up server.js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,20 @@
-const express = require('express');
+const express = require("express");
+
 const server = express();
 
+const actionsRouter = require("./actions/actions-router");
+const projectsRouter = require("./projects/projects-router");
+
+server.use(express.json());
+
 // Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+
+server.use(errorHandling);
 
 module.exports = server;
+
+function errorHandling(err, req, res, next) {
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+}
