0bfe1fbf89b8a6f281afbbe8bce2269fc59d561d
MMostella <masonmostella@gmail.com>
2021-09-30 18:59:00 -0500

    Built and plugged in logger

--- api/server.js
+++ api/server.js
@@ -6,6 +6,8 @@ const actionsRouter = require("./actions/actions-router");
 const projectsRouter = require("./projects/projects-router");
 
 server.use(express.json());
+server.use("/api/actions", logger, actionsRouter);
+server.use("api/projects", logger, projectsRouter);
 
 // Configure your server here
 
@@ -17,4 +19,13 @@ function errorHandling(err, req, res, next) {
   res.status(err.status || 500).json({
     message: err.message,
   });
+}
+
+function logger(req, res, next) {
+  console.log(
+    `${new Date().toLocaleTimeString()} it is a ${req.method} request to ${
+      req.originalUrl
+    }`
+  );
+  next();
 }
